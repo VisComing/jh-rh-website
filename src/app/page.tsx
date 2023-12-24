f44e29ae977a1fcb73abab82cc3deb052ec06aa4
zhouruifa <zhouruifa@bytedance.com>
2023-12-24 21:55:27 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,10 @@ export default function Home() {
       </span>
       <p>video: https://www.youtube.com/watch?v=88QJumITbQA</p>
       <p>react框架：https://react.docschina.org/learn</p>
-      <p>nextjs: https://nextjs.org/docs</p>
+      <p>
+        nextjs:
+        https://nextjs.org/learn/react-foundations/what-is-react-and-nextjs
+      </p>
       <p>组件库：https://ant.design/components/overview-cn</p>
       <p>网站模板：https://pro.ant.design/zh-CN/docs/getting-started/</p>
       <span>启动项目，vscode，打开终端，输入npm run dev</span>
