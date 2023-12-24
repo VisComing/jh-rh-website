e5a257fa75a80a89eca080da21b06d2e4d3b2d50
chengjianghui <13822360+chengjianghui@user.noreply.gitee.com>
2023-12-24 18:54:17 +0800

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>博客</h1>
+      <span>JavaScript基础：https://developer.mozilla.org/zh-CN/docs/learn/JavaScript</span>
+      <p>video: https://www.youtube.com/watch?v=88QJumITbQA</p>
+      <p>react框架：https://react.docschina.org/learn</p>
+      <p>nextjs: https://nextjs.org/docs</p>
+      <p>组件库：https://ant.design/components/overview-cn</p>
+      <p>网站模板：https://pro.ant.design/zh-CN/docs/getting-started/</p>
+      <span>启动项目，vscode，打开终端，输入npx next dev</span>
+      <br />
+      <MyButton />
+    </div>
+  )
+}
+
+function MyButton() {
+  return (
+    <button>Im a button</button>
+  );
+}
