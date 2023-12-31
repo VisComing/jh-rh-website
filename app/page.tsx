13d2fbf7c961eb04e59d1b4ef904b13cc91ed63e
zhouruifa <zhouruifa@bytedance.com>
2023-12-31 20:18:08 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
 "use client";
 
 import { Button, Card } from "antd";
-import "./card_css.css";
 
 const MyCard: React.FC<{
   title: string;
   desc: string;
-  url: string;
+  url?: string;
 }> = ({ title, desc, url }) => (
   <Card
-    className="my-card"
     style={{ width: 300 }}
-    title={<h3 className="my-card-title">{title}</h3>}
+    title={<h3>{title}</h3>}
     onClick={() => {
-      open(url);
+      Boolean(url) && open(url);
     }}
   >
     {desc}
@@ -22,35 +20,33 @@ const MyCard: React.FC<{
 
 export default function Home() {
   return (
-    <div>
-      <h1>博客</h1>
-      <span>
-        JavaScript基础：https://developer.mozilla.org/zh-CN/docs/learn/JavaScript
-      </span>
-      <p>video: https://www.youtube.com/watch?v=88QJumITbQA</p>
-      <p>react框架：https://react.docschina.org/learn</p>
-      <p>
-        nextjs:
-        https://nextjs.org/learn/react-foundations/what-is-react-and-nextjs
-      </p>
-      <p>组件库：https://ant.design/components/overview-cn</p>
-      <p>网站模板：https://pro.ant.design/zh-CN/docs/getting-started/</p>
-      <span>启动项目，vscode，打开终端，输入npm run dev</span>
-      <br />
-      <MyButton />
+    <div className="grid grid-cols-4 gap-4 mt-10 ml-10">
       <MyCard
         title="JavaScript基础"
         desc="从MDN官网获取有关JavaScript的详细信息"
         url="https://developer.mozilla.org/zh-CN/docs/learn/JavaScript"
       />
+      <MyCard
+        title="前端视频教程"
+        desc="从YouTube获取最优秀的视频内容"
+        url="https://www.youtube.com/watch?v=88QJumITbQA"
+      />
+      <MyCard
+        title="React框架"
+        desc="从react.dev官网获取最新最易懂的教程"
+        url="https://react.docschina.org/learn"
+      />
+      <MyCard
+        title="Next.js"
+        desc="从netx官网学习目前最流行的react框架"
+        url="https://nextjs.org/learn/react-foundations/what-is-react-and-nextjs"
+      />
+      <MyCard
+        title="Ant design"
+        desc="从ant design官网学习组件库"
+        url="https://ant.design/components/overview-cn"
+      />
+      <MyCard title="如何启动项目" desc="vscode，打开终端，输入npm run dev" />
     </div>
-  );
-}
-
-function MyButton() {
-  return (
-    <Button onClick={() => alert("你好，江慧~")} type="primary">
-      Im a button
-    </Button>
   );
 }
